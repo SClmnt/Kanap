85abd231264a3ad18d8511b6814c757132d5630b
clement silone <silone.clement@gmail.com>
2022-12-13 22:53:02 +0100

    js

--- front/js/script.js
+++ front/js/script.js
@@ -9,30 +9,39 @@ fetch ("http://localhost:3001/api/products")
     })
     .catch(function(err){
 
-    })
-    .then( data => data.json)
-;
+    });
 
-class product {
-    constructor(color, id, name, price, imageUrl, description, altTxt){
-        this.color = color;
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.altTxt = altTxt;
-    }
+function products(data){
+
+    let product =
+    `<a href="./product.html?id=42">
+        <article>
+            <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+            <h3 class="productName">Kanap name1</h3>
+            <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+        </article>
+    </a>`;
+        for (let r of data.list) {
+            product +=
+            `<a href="./product.html?id=${r._id}">
+            <article>
+                <img src="${r.imageUrl}" alt="${r.altTxt}">
+                <h3 class="productName">${r.name}</h3>
+                <p class="productDescription">${r.description}</p>
+            </article>
+        </a>`;
+        }
+        document.getElementById("items").innerHTML = product;
+    
 }
 
 
+//const productIndex = document.getElementById("items");
+//productIndex.innerHTML = `<a href="./product.html?id=42">
+//<article>
+ // <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+//  <h3 class="productName">Kanap name1</h3>
+ // <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
+//</article>
+//</a>`;
 
-
-const productIndex = document.getElementById("items");
-productIndex.innerHTML = `<a href="./product.html?id=42">
-<article>
-  <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-  <h3 class="productName">Kanap name1</h3>
-  <p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>
-</article>
-</a>`;
