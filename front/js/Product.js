c64a50e9118dcceed8f3326a6f766aac54d3e9b5
clement silone <silone.clement@gmail.com>
2022-12-15 19:05:30 +0100

    Etape 6 finie

--- front/js/Product.js
+++ front/js/Product.js
@@ -1,27 +1,49 @@
-fetch ("http://localhost:3001/api/products")
+
+    var currentUrl = window.location.href;
+    var url = new URL(currentUrl);
+    var urlId = url.searchParams.get("id");
+
+
+/*http://localhost:3001/api/products/${urlId}*/
+
+fetch (`http://localhost:3001/api/products/${urlId}`)
     .then(function(res){
         if(res.ok)  {
             return res.json();
         }
     })
     .then(function(value){
-        console.log(value);
-        productsId(value);
+
         productFill(value);
+        console.log('toto');
+        colors(value);
+        console.log('tata');
     })
     
     .catch(function(err){
 
     });
 
+function productFill (data){
+    let title = document.querySelector("title");
+    let productImg = document.getElementsByClassName("item__img");
+    let productName = document.getElementById("title");
+    let productPrice = document.getElementById("price");
+    let productDescription = document.getElementById("description");
 
 
-        var currentUrl = window.location.href;
-        var url = new URL(currentUrl);
-        var urlId = url.searchParams.get("id");
-        var productNumber = 0;
-        var productId = productsList[productNumber]._id;
+    title.innerText = `${data.name}`;
+    productImg[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+    productName.innerText = `${data.name}`;
+    productPrice.innerText = `${data.price}`;
+    productDescription.innerText = `${data.description}`;
+   
+};
 
-get (urlId)
+function colors(data){
+    let productColors = document.getElementById("colors");
     
-    
+    for (i = 0; i < data.colors.length; i++){
+    productColors.innerHTML +=`<option value="${data.colors[i]}">${data.colors[i]}</option>`;
+    console.log(productColors);}
+};
